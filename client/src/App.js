2b3ebf2399d06675e3cf7fa935c8bfb4e73636af
Tim Quattrochi <timq82@gmail.com>
2023-05-09 17:26:54 -0400

    register and login, minor styling, just need to redirect, handle user state

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
