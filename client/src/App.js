8b4f220a00edfff1ae46d5a08d34033a593d301c
Nathaniel Grandinette <nathaniel.grandinette@gmail.com>
2023-05-09 19:34:26 -0700

    Added productcard and product list page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
+import ProductList from './components/Products/ProductList'
+import ProductDetail from "./components/Products/ProductDetail";
+import ProductCards from "./components/Products/ProductCards";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path='/productlist' element={<ProductList />} />
+        <Route path='/productdetail' element={<ProductDetail />} />
+        <Route path='/productcard' element={<ProductCards />} />
       </Routes>
     </div>
   );
