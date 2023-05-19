35447c76e4f0fc80edfda3d6c4e95173c8f7cbd5
Tim Quattrochi <timq82@gmail.com>
2023-05-18 23:20:26 -0400

    hide btn if user is not admin

--- client/src/components/Products/ProductList.js
+++ client/src/components/Products/ProductList.js
@@ -1,47 +1,64 @@
-import React, { useState, useEffect, useContext } from 'react'
-import Product from './Product'
-import axios from '../../utils/axiosConfig'
-import './productlist.css'
+import React, { useState, useEffect, useContext } from "react";
+import Product from "./Product";
+import axios from "../../utils/axiosConfig";
+import "./productlist.css";
 import { AuthContext } from "../../context/AuthContext";
 
 const ProductList = () => {
-    const [products, setProducts] = useState()
-    const [refresh, setRefresh] = useState(false)
-    const { user } = useContext(AuthContext);
+  const [products, setProducts] = useState();
+  const [refresh, setRefresh] = useState(false);
 
-    const getProducts = async () => {
-        const productList = await axios.get('/product')
-        setProducts(productList.data)
-    }
+  const { user } = useContext(AuthContext);
 
-    useEffect(() => {
-        getProducts()
-    }, [refresh])
+  const getProducts = async () => {
+    const productList = await axios.get("/product");
+    setProducts(productList.data);
+  };
 
-    return (
-        <div>
-            <h2>Products</h2>
-            <h2>Hello, {user?.name}</h2>
-            <div className="product-list-cards">
-                {products && products.map((product) => {
-                    return (
-                        <Product key={product._id} user={user} setRefresh={setRefresh} refresh={refresh} product={product} getProducts={getProducts} />
-                    )
-                })}
-            </div>
-            <div className="w-full product-list-button">
-                <button className="bg-green-600 p-2 w-1/3 rounded" onClick={(e) => {
-                    setProducts([...products, {
-                        name: "",
-                        price: "",
-                        description: "",
-                        quantity: ""
-                    }])
-                }}>Add Product</button>
-            </div>
+  useEffect(() => {
+    getProducts();
+  }, [refresh]);
 
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2>Products</h2>
+      <h2>Hello, {user?.name}</h2>
+      <div className="product-list-cards">
+        {products &&
+          products.map((product) => {
+            return (
+              <Product
+                key={product._id}
+                user={user}
+                setRefresh={setRefresh}
+                refresh={refresh}
+                product={product}
+                getProducts={getProducts}
+              />
+            );
+          })}
+      </div>
+      <div className="w-full product-list-button">
+        <button
+          hidden={user?.role === "user"} //hide if user is not admin.
+          className="bg-green-600 p-2 w-1/3 rounded"
+          onClick={(e) => {
+            setProducts([
+              ...products,
+              {
+                name: "",
+                price: "",
+                description: "",
+                quantity: "",
+              },
+            ]);
+          }}
+        >
+          Add Product
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default ProductList
+export default ProductList;
