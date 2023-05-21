2a19835b9c7c57466db62419ac753d0bd3aff87b
Tim Quattrochi <timq82@gmail.com>
2023-05-20 22:08:12 -0400

    nav bar, conditional nav links, react icons

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,17 @@ import ProductCards from "./components/ProductCards";
 import Login from "./pages/Login";
 import Users from "./components/Users/Users";
 import Landing from "./pages/Landing";
-import Home from './pages/Home/Home'
+import Home from "./pages/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div>
+      <NavBar />
+
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path='/home' element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/productlist" element={<ProductList />} />
         <Route
--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -2,76 +2,79 @@ import React, { useState, useEffect, useContext } from "react";
 import Product from "./Product";
 import axios from "../utils/axiosConfig";
 import "./productlist.css";
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { AuthContext } from "../context/AuthContext";
 
 const ProductList = () => {
-    const [products, setProducts] = useState();
-    const [refresh, setRefresh] = useState(false);
+  const [products, setProducts] = useState();
+  const [refresh, setRefresh] = useState(false);
 
-    const { user } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
-    const getProducts = async () => {
-        const productList = await axios.get("/product");
-        setProducts(productList.data);
-    };
+  const getProducts = async () => {
+    const productList = await axios.get("/product");
+    setProducts(productList.data);
+  };
 
-    useEffect(() => {
-        getProducts();
-    }, [refresh]);
-    console.log(window.location.pathname)
-    return (
-        <div>
-            <ToastContainer
-                position="top-center"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                pauseOnHover
-                theme="dark"
-            />
-            <h2>Products</h2>
-            <h2>Hello, {user?.name}</h2>
-            <div className="product-list-cards">
-                {products &&
-                    products.map((product) => {
-                        return (
-                            <Product
-                                key={product._id}
-                                user={user}
-                                setRefresh={setRefresh}
-                                refresh={refresh}
-                                product={product}
-                                getProducts={getProducts}
-                                toast={toast}
-                            />
-                        );
-                    })}
-            </div>
-            {window.location.pathname !== '/home' ? 
-            <div className="w-full product-list-button">
-                <button
-                    hidden={user?.role === "user"} //hide if user is not admin.
-                    className="bg-green-600 p-2 w-1/3 rounded"
-                    onClick={(e) => {
-                        setProducts([
-                            ...products,
-                            {
-                                name: "",
-                                price: "",
-                                description: "",
-                                quantity: "",
-                            },
-                        ]);
-                    }}
-                >
-                    Add Product
-                </button>
-            </div> : "" }
+  useEffect(() => {
+    getProducts();
+  }, [refresh]);
+
+  return (
+    <div>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnHover
+        theme="dark"
+      />
+      <h2>Products</h2>
+      <h2>Hello, {user?.name}</h2>
+      <div className="product-list-cards">
+        {products &&
+          products.map((product) => {
+            return (
+              <Product
+                key={product._id}
+                user={user}
+                setRefresh={setRefresh}
+                refresh={refresh}
+                product={product}
+                getProducts={getProducts}
+                toast={toast}
+              />
+            );
+          })}
+      </div>
+      {window.location.pathname !== "/home" ? (
+        <div className="w-full product-list-button">
+          <button
+            hidden={user?.role === "user"} //hide if user is not admin.
+            className="bg-green-600 p-2 w-1/3 rounded"
+            onClick={(e) => {
+              setProducts([
+                ...products,
+                {
+                  name: "",
+                  price: "",
+                  description: "",
+                  quantity: "",
+                },
+              ]);
+            }}
+          >
+            Add Product
+          </button>
         </div>
-    );
+      ) : (
+        ""
+      )}
+    </div>
+  );
 };
 
 export default ProductList;
