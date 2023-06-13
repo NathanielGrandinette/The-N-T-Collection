745b925478f608ddc4ec1620f1396dd9b2f25947
Tim Quattrochi <timq82@gmail.com>
2023-06-12 23:52:46 -0400

    checks if item is already in wishlist

--- server/routes/wishlist.js
+++ server/routes/wishlist.js
@@ -2,16 +2,29 @@ const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middleware/auth");
 const User = require("../models/User");
+
 router.route("/").put(verifyToken, async (req, res, next) => {
   const { product } = req.body;
 
   const { user_id } = req.user;
 
   try {
+    const currUser = await User.findOne({ _id: user_id });
+
+    const checkIfItemIsWished = currUser.wishList.find(
+      (item) => item.product._id.toString() === product._id.toString()
+    );
+
+    if (checkIfItemIsWished) {
+      return res
+        .status(400)
+        .json({ error: "Item is already in wishlist." });
+    }
+
     const user = await User.findByIdAndUpdate(
       user_id,
       {
-        $push: { wishList: { product: product._id } },
+        $addToSet: { wishList: { product: product._id } },
       },
       { new: true }
     )
