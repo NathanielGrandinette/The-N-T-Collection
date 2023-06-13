742c01410c6d5576c8e32ddbc3d0b4607cc0f12f
Tim Quattrochi <timq82@gmail.com>
2023-06-13 17:21:42 -0400

    sent the image photo in response to wishlist

--- server/routes/wishlist.js
+++ server/routes/wishlist.js
@@ -3,45 +3,78 @@ const router = express.Router();
 const verifyToken = require("../middleware/auth");
 const User = require("../models/User");
 
-router.route("/").put(verifyToken, async (req, res, next) => {
-  const { product } = req.body;
+router
+  .route("/")
+  .get(verifyToken, async (req, res, next) => {
+    const { user_id } = req.user;
 
-  const { user_id } = req.user;
+    try {
+      const currUser = await User.findOne({ _id: user_id }).populate({
+        path: "wishList.product",
+        select: " photo.path name price description updatedAt",
+      });
 
-  try {
-    const currUser = await User.findOne({ _id: user_id });
+      const wishListItems = currUser.wishList.map((item) => {
+        const { product, dateAdded, _id } = item ?? {};
+        const { name, price, photo } = product ?? {};
+        const { path } = photo;
+        return {
+          productId: product?.id,
+          productName: name,
+          productPrice: price,
+          dateAdded,
+          wishListItemId: _id,
+          photo: path,
+        };
+      });
 
-    const checkIfItemIsWished = currUser.wishList.find(
-      (item) => item.product._id.toString() === product._id.toString()
-    );
-
-    if (checkIfItemIsWished) {
-      return res
-        .status(400)
-        .json({ error: "Item is already in wishlist." });
+      return res.status(200).send(wishListItems);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send({ error: "Something went wrong" });
     }
+  })
 
-    const user = await User.findByIdAndUpdate(
-      user_id,
-      {
-        $addToSet: { wishList: { product: product._id } },
-      },
-      { new: true }
-    )
-      .populate({
-        path: "wishList",
+  .put(verifyToken, async (req, res, next) => {
+    const { product } = req.body;
 
-        populate: {
-          path: "product",
-          select: "name price description updatedAt",
+    const { user_id } = req.user;
+
+    try {
+      const currUser = await User.findOne({ _id: user_id });
+
+      const checkIfItemIsWished = currUser.wishList.find(
+        (item) =>
+          item.product._id.toString() === product._id.toString()
+      );
+
+      if (checkIfItemIsWished) {
+        return res
+          .status(400)
+          .json({ error: "Item is already in wishlist." });
+      }
+
+      const user = await User.findByIdAndUpdate(
+        user_id,
+        {
+          $addToSet: { wishList: { product: product._id } },
         },
-      })
-      .select("-password");
+        { new: true }
+      )
+        .populate({
+          path: "wishList",
 
-    return res.status(200).send(user.wishList);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send({ error: "Something went wrong" });
-  }
-});
+          populate: {
+            path: "product",
+            select: " photo.path name price description updatedAt",
+          },
+        })
+        .select("-password");
+
+      return res.status(200).send(user.wishList);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send({ error: "Something went wrong" });
+    }
+  });
 module.exports = router;
