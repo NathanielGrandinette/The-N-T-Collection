826323807e1791de6b0a717ce2877e1c3d159480
Nathaniel Grandinette <nathaniel.grandinette@gmail.com>
2023-06-14 19:10:06 -0700

    Added styling and forms to checkout page

--- client/src/pages/Checkout/Checkout.js
+++ client/src/pages/Checkout/Checkout.js
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from 'react'
 import './Checkout.css'
 import { useCartContext } from '../../context/CartContex';
 import { useNavigate } from 'react-router-dom';
+import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
+import CheckoutForm from './CheckoutForm';
 
 const Checkout = () => {
-
+    const [loading, setLoading] = useState(false)
+    const [address, setAddress] = useState()
+    const [payment, setPayment] = useState()
     const { cart, getCart } = useCartContext();
 
     const navigate = useNavigate()
@@ -17,27 +21,56 @@ const Checkout = () => {
             return accumulator + value
         })
     }
+
     getTotal()
-    console.log(cart)    
+
+    const confirmOrder = () => {
+        setLoading(true)
+        setTimeout(() => {
+            console.log("test")
+            setLoading(false)
+        }, 1000)
+    }
+
     return (
         <div className="checkout">
-            {cart && cart.items?.map((item) => {
-                return (
-                    <div className="checkout-item">
-                        <div className="checkout-item-img-name">
-                            <img src={item.photo.path}></img>
-                            <div>{item.name}</div>
+            <CheckoutForm
+                address={address}
+                setAddress={setAddress}
+                payment={payment}
+                setPayment={setPayment}
+            />
+            <div style={{display: 'flex', flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+                {cart && cart.items?.map((item) => {
+                    return (
+                        <div className="checkout-item">
+                            <div className="checkout-item-img-name">
+                                <img src={item.photo.path || item.photo}></img>
+                                <h2>{item.name}</h2>
+                            </div>
+                            <div className="quantity-price">
+                                <div>Quantity: {item.shopped}</div>
+                                <div>${item.price * item.shopped}</div>
+                            </div>
                         </div>
-                        <div>
-                            <div>Quantity: {item.shopped}</div>
-                            <div>${item.price * item.shopped}</div>
+                    )
+                })}
+                <div style={{ height: "200px", width: '75%' }}>
+                    <div className="checkout-button-total">
+                        <button
+                            onClick={() => confirmOrder()}
+                            className="block bg-slate-500 text-white hover:bg-slate-700 uppercase p-2 rounded">
+                            {loading ? <LoadingSpinner /> : 'Confirm Order'}
+                        </button>
+                        <div className="checkout-total">
+                            Order Total: ${total}
                         </div>
                     </div>
-                )
-            })}
-            <div className="checkout-total">
-                Total: ${total}
+                </div>
             </div>
+
+
+
         </div>
     )
 }
