ebcd33fac4070f56c3ee0ab2df8efd69c5a63101
Nathaniel Grandinette <nathaniel.grandinette@gmail.com>
2023-06-22 19:10:28 -0700

    Created and styled order confirmation page

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -13,6 +13,7 @@ const verifyToken = (req, res, next) => {
     const decoded = jwt.verify(token, keys.jwt.secret);
     req.user = decoded;
   } catch (error) {
+    console.log(error)
     return res.status(401).send("Invalid Token");
   }
   return next();
