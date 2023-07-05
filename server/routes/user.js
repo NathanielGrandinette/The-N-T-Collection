d91ed8fe357fb2ec9c604fd5576b540ccb8c31eb
Nathaniel Grandinette <nathaniel.grandinette@gmail.com>
2023-07-04 19:48:00 -0700

    Pulling changes Pulling changes

--- server/routes/user.js
+++ server/routes/user.js
@@ -114,87 +114,87 @@ router.post("/login", validateBodyParams("email", "password"), async (req, res) 
 });
 
 //@POST http://localhost:3001/user/register
-router.post("/register", validateBodyParams("name", "email", "password", "confirmPassword"), async (req, res) => {
-  const { name, email, password, confirmPassword } = req.body;
+// router.post("/register", validateBodyParams("name", "email", "password", "confirmPassword"), async (req, res) => {
+//   const { name, email, password, confirmPassword } = req.body;
 
-  const { user_id } = req.user;
-  const { id: userIdFromParams } = req.params;
+//   const { user_id } = req.user;
+//   const { id: userIdFromParams } = req.params;
 
-  if (
-    !name ||
-    !password ||
-    !confirmPassword ||
-    !email ||
-    !streetAddress ||
-    !city ||
-    !zip ||
-    !state ||
-    !currPassword
-  ) {
-    return res
-      .status(422)
-      .json({ error: "Please fill out all required fields." });
-  } else if (password !== confirmPassword) {
-    return res.status(422).json({ error: "Passwords must match." });
-  }
-  try {
-    // check the user id from the params against the user id in the db first
-    const checkUser = await User.findById(userIdFromParams);
+//   if (
+//     !name ||
+//     !password ||
+//     !confirmPassword ||
+//     !email ||
+//     !streetAddress ||
+//     !city ||
+//     !zip ||
+//     !state ||
+//     !currPassword
+//   ) {
+//     return res
+//       .status(422)
+//       .json({ error: "Please fill out all required fields." });
+//   } else if (password !== confirmPassword) {
+//     return res.status(422).json({ error: "Passwords must match." });
+//   }
+//   try {
+//     // check the user id from the params against the user id in the db first
+//     const checkUser = await User.findById(userIdFromParams);
 
-    if (user_id.toString() !== checkUser._id.toString()) {
-      return res.status(403).json({ error: "Forbidden." });
-    }
+//     if (user_id.toString() !== checkUser._id.toString()) {
+//       return res.status(403).json({ error: "Forbidden." });
+//     }
 
-    const checkCurrPwd = await bcrypt.compare(
-      currPassword,
-      checkUser.password
-    );
+//     const checkCurrPwd = await bcrypt.compare(
+//       currPassword,
+//       checkUser.password
+//     );
 
-    //check that the current password matches the password in the db.
-    if (!checkCurrPwd) {
-      return res.status(401).json({ error: "Invalid credentials." });
-    }
+//     //check that the current password matches the password in the db.
+//     if (!checkCurrPwd) {
+//       return res.status(401).json({ error: "Invalid credentials." });
+//     }
 
-    //check that the new password isn't the same as the current password in the db.
-    const checkPrevPwd = await bcrypt.compare(
-      confirmPassword,
-      checkUser.password
-    );
-    if (checkPrevPwd) {
-      return res.status(400).json({
-        error:
-          "Cannot use the same password as your current password.",
-      });
-    }
+//     //check that the new password isn't the same as the current password in the db.
+//     const checkPrevPwd = await bcrypt.compare(
+//       confirmPassword,
+//       checkUser.password
+//     );
+//     if (checkPrevPwd) {
+//       return res.status(400).json({
+//         error:
+//           "Cannot use the same password as your current password.",
+//       });
+//     }
 
-    //hash the new password
-    const hashedPassword = await bcrypt.hash(password, 10);
+//     //hash the new password
+//     const hashedPassword = await bcrypt.hash(password, 10);
 
-    let user = await User.findById(user_id).populate("address");
+//     let user = await User.findById(user_id).populate("address");
 
-    user.name = name;
-    user.password = hashedPassword;
-    user.email = email;
+//     user.name = name;
+//     user.password = hashedPassword;
+//     user.email = email;
 
-    const address = await Address.findByIdAndUpdate(addressId, {
-      address: streetAddress,
-      addressLine1: addressLine1 ? addressLine1 : null,
-      city,
-      state,
-      zip,
-    });
+//     const address = await Address.findByIdAndUpdate(addressId, {
+//       address: streetAddress,
+//       addressLine1: addressLine1 ? addressLine1 : null,
+//       city,
+//       state,
+//       zip,
+//     });
 
-    user.address = address._id;
+//     user.address = address._id;
 
-    await user.save();
-    user = user.toJSON();
-    delete user.password;
-    return res.status(200).json(user);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error });
-  }
-});
+//     await user.save();
+//     user = user.toJSON();
+//     delete user.password;
+//     return res.status(200).json(user);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ error: error });
+//   }
+// });
 
 //@POST http://localhost:3001/user/register
 router.post("/register", validateBodyParams("name", "email", "password", "confirmPassword"), async (req, res) => {
