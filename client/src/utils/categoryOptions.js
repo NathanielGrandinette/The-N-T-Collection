4225f566a445fec808edb872920ac25485eb2f04
Tim Quattrochi <timq82@gmail.com>
2023-07-10 00:52:02 -0400

    Select component with cat options

---
+++ client/src/utils/categoryOptions.js
@@ -0,0 +1,12 @@
+export const categoryOptions = [
+  { value: "", text: "--Select Category--" },
+  { value: "Electronics", text: "Electronics" },
+  { value: "Toys", text: "Toys" },
+  { value: "Automotive", text: "Automotive" },
+  { value: "Pets", text: "Pets" },
+  { value: "Health and Beauty", text: "Health and Beauty" },
+  { value: "Clothing", text: "Clothing" },
+  { value: "Jewelry", text: "Jewelry" },
+  { value: "General Household", text: "General Household" },
+  { value: "Other", text: "Other" },
+];
