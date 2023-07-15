3d4c856682318b7a556ffaeba476cdd668a572bf
Tim Quattrochi <timq82@gmail.com>
2023-07-15 18:56:03 -0400

    if the user is logged out because of a valid token, redirect to login

--- client/src/hooks/useWishList.js
+++ client/src/hooks/useWishList.js
@@ -1,10 +1,13 @@
 import { useState, useEffect, useContext } from "react";
 import axios from "../utils/axiosConfig";
+import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import { toast } from "react-toastify";
 
 function useWishList() {
   const [wishList, setWishList] = useState(null);
+
+  const navigate = useNavigate();
 
   const user = useContext(AuthContext);
 
@@ -15,6 +18,12 @@ function useWishList() {
         setWishList(list.data);
       } catch (error) {
         console.log(error);
+        if (
+          error.response.data === "Invalid Token" ||
+          error.response.status === 401
+        ) {
+          navigate("/login", { replace: true });
+        }
         console.error("From useWishList: Error with user token.");
       }
     } else {
