ae28681cd74950437ea940c6364bd07acc744a4f
Tim Quattrochi <timq82@gmail.com>
2023-07-15 18:55:13 -0400

    comments

--- server/routes/product.js
+++ server/routes/product.js
@@ -15,6 +15,10 @@ const populateProductOwner = {
   select: "email",
 };
 
+/**
+ * GET /product
+ * @description an endpoint to get all the products.
+ */
 router
   .route("/")
   .get(async (req, res, next) => {
@@ -27,7 +31,10 @@ router
       next();
     }
   })
-
+  /**
+   * POST /product
+   * Protected Route
+   */
   .post(
     verifyToken,
     verifyRole(["admin"]),
@@ -35,7 +42,8 @@ router
     async (req, res, next) => {
       const { name, price, description, quantity, category } =
         req.body;
-
+      //If the these fields are undefined, it means the admin clicked "cancel" when they were adding a new product
+      //This is a side effect so let's just send a message right away, to give some user notification instead of just being an error.
       if (
         name === "undefined" ||
         price === "undefined" ||
@@ -65,6 +73,20 @@ router
           //upload image to cloudinary
           const imageResult = await cloudinary.uploader.upload(path, {
             public_id: name, // keep product name in cloudinary file name.
+            transformation: [
+              {
+                dpr: "auto",
+                responsive: true,
+                width: "auto",
+                crop: "scale",
+                angle: 20,
+                fetch_format: "webp",
+              },
+              {
+                border: "3px_solid_rgb:00390b",
+                radius: 20,
+              },
+            ],
           });
 
           if (checkForProduct) {
@@ -94,7 +116,10 @@ router
       }
     }
   );
-
+/**
+ * GET /product/:id
+ * @description Endpoint to get a product by it's id.
+ */
 router
   .route("/:id")
   .get(async (req, res, next) => {
@@ -115,6 +140,11 @@ router
       next();
     }
   })
+  /**
+   * PUT /product/:id
+   * @description Endpoint to edit a product. If there is a req.file then update the whole product, if not then only the fields provided.
+   * Protected
+   */
   .put(
     verifyToken,
     verifyRole(["admin"]),
@@ -133,14 +163,19 @@ router
       try {
         let product = await Product.findById(id);
 
-        if (process.env.NODE_ENV !== "production") {
-          //used in production, easier to test other products in dev.
-          if (product.productOwner.toString() !== userId) {
-            return res
-              .status(401)
-              .json({ error: "You can only edit your products." });
-          }
-        }
+        /**
+         * Commented out on purpose
+         * If this app is in production
+         * Only the product owner can edit the product
+         */
+        // if (process.env.NODE_ENV === "production") {
+        //   //used in production, easier to test other products in dev.
+        //   if (product.productOwner.toString() !== userId) {
+        //     return res
+        //       .status(401)
+        //       .json({ error: "You can only edit your products." });
+        //   }
+        // }
 
         const checkIsAdmin = await User.findOne({ _id: userId });
 
@@ -155,25 +190,43 @@ router
           //upload image to cloudinary
           const imageResult = await cloudinary.uploader.upload(path, {
             public_id: name, // keep product name in cloudinary file name.
+            overwrite: true,
+            transformation: [
+              {
+                dpr: "auto",
+                responsive: true,
+                width: "auto",
+                crop: "scale",
+                fetch_format: "webp",
+              },
+              {
+                border: "3px_solid_rgb:00390b",
+                radius: 20,
+              },
+            ],
           });
 
-          const updateProduct = await Product.findByIdAndUpdate(id, {
-            name,
-            price,
-            description,
-            category,
-            quantity,
-            photo: {
-              name: originalname,
-              path: imageResult.secure_url,
-              contentType: mimetype,
-              cloudinaryId: imageResult.public_id,
-            },
-          });
-
-          deleteProductImage(product); // delete the previous product photo.
-
-          return res.status(200).send(updateProduct);
+          if (imageResult) {
+            const updateProduct = await Product.findByIdAndUpdate(
+              id,
+              {
+                name,
+                price,
+                description,
+                category,
+                quantity,
+                photo: {
+                  name: originalname,
+                  path: imageResult.secure_url,
+                  contentType: mimetype,
+                  cloudinaryId: imageResult.public_id,
+                },
+              }
+            );
+            return res.status(200).send(updateProduct);
+          } else {
+            res.status(500).json({ error: "Something went wrong." });
+          }
         }
 
         if (price) {
@@ -205,6 +258,10 @@ router
       }
     }
   )
+  /**
+   * DELETE /product/:id
+   * Protected
+   */
   .delete(
     verifyToken,
     verifyRole(["admin"]),
@@ -236,12 +293,10 @@ router
       }
     }
   );
-
-/*
-@PUT
-@ Protected - Admin
-@ /product/featured
-*/
+/**
+ * PUT /product/featured/:productId
+ * Protected
+ */
 router.route("/featured/:productId").put(
   verifyToken,
   verifyRole(["admin"]),
